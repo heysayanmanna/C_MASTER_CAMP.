214dd28cf1ab972d5f5b2cf15569d5c45738b8d6
hey sayanmanna <sayanmanna.in@gmail.com>
2025-12-12 13:18:01 +0530

    First upload

---
+++ Basic Of C/Program_structure.c
@@ -0,0 +1,24 @@
+
+/* Basic structure of a C program
+ This program prints "Hello, World!" to the console
+A C program must have a main function as the entry point */
+
+
+#include<stdio.h> // Include standard input-output header file
+                 // This is a preprocessor directive
+
+int main() {    // Main function - execution starts here
+                // Return type is int
+                // No parameters for main function
+                // Function body starts here
+                // Curly braces define the scope of the function
+                // Statements end with a semicolon
+
+    printf("Hello, World!\n"); // Print function to display text on the console
+                             // \n is a newline character   
+                             // Function call statement
+
+    return 0;               // Return statement
+                             // Indicates that the program ended successfully
+
+}
